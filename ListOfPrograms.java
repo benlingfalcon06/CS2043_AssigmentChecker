83de0d5726f7b9c530d2db9484510acf246bb8bf
fmsl1380 <farbod.mosallaei@gmail.com>
2025-11-20 14:46:51 -0400

    Update ListOfPrograms.java

--- ListOfPrograms.java
+++ ListOfPrograms.java
@@ -1,1 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListOfPrograms {
+    private final List<Program> programs = new ArrayList<>();
+
+    public void add(Program p) {
+        programs.add(p);
+    }
+
+    public int size() {
+        return programs.size();
+    }
+
+    public Program get(int index) {
+        return programs.get(index);
+    }
+
+    public List<Program> asList() {
+        return programs;
+    }
+}
 
