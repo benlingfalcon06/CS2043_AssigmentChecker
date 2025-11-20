7c63576ed6f18b3d7d3dc89e73fe14547b1e25cf
Emmyk-17 <136089110+Emmyk-17@users.noreply.github.com>
2025-11-20 01:34:57 +0100

    intial upload of the testcase file
    This state has no function or varibles working just the basic structures

--- TestCase.java
+++ TestCase.java
@@ -1,1 +1,31 @@
+import java.io.FileWriter;
+import java.io.IOException;
 
+/*****************************************************
+ * TestCase for test running the code
+ * 
+ * Author: Emmanuel Adeyemi-Kings
+ * Date: 19th Novemeber 2025
+ * Title: Software Engineering project submission 3
+ ***************************************************/
+public class TestCase
+{
+    private FileWriter tc;
+    private String input;
+    
+    public TestCase(String inputTC)
+    {
+       
+    }
+    
+    public static void create()
+    {
+        
+    }
+    
+    //For test
+    public static void main(String[] args)
+    {
+        
+    }
+}
