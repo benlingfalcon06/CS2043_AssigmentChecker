2a0f12141447d3800a8bd10b25735f5cc4c6b90b
Atharva <naikatharva28@gmail.com>
2025-11-20 13:23:37 -0400

    addition of coordinator and program classes, need to work on working classes, looks complicated for now

--- Coordinator.java
+++ Coordinator.java
@@ -1,1 +1,35 @@
+/********************************************
+ * Coordinator class connects the UI to the
+ * backened logic of the program.
+ * Everyone needs connect their respectivly
+ * assigned classes to this class.
+ * 
+ * 
+ * Verison 1.0
+ * @author Group 8
+ */
 
+
+public class Coordinator 
+{
+    private ListOfPrograms programs;
+    private ListOfTestSuites suites;
+
+    public Coordinator(ListOfPrograms programs, ListOfTestSuites suites) 
+    {
+        this.programs = programs;
+        this.suites = suites;
+    }
+
+    public void runSuiteOnProgram(String suiteName, Program p) 
+    {
+        TestSuite suite = suites.find(suiteName);
+
+        for (TestCase tc : suite.getTestCases().getAll()) 
+        {
+            String actual = p.runOnInput(tc.getInput());
+            boolean passed = tc.compareOutput(actual);
+            System.out.println(tc.getName() + ": " + (passed ? "PASS" : "FAIL"));
+        }
+    }
+}
