75c5fa7a7e4410218a8c77d044f8ca3e40202e73
fmsl1380 <farbod.mosallaei@gmail.com>
2025-11-20 14:42:34 -0400

    Update ListOfTestCase.java
    class of ListOfTesrCases

--- ListOfTestCase.java
+++ ListOfTestCase.java
@@ -1,4 +1,23 @@
-public class ListOfTestCase 
-{
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListOfTestCase {
+    private final List<TestCase> cases = new ArrayList<>();
+
+    public void add(TestCase tc) {
+        cases.add(tc);
+    }
+
+    public int size() {
+        return cases.size();
+    }
+
+    public TestCase get(int index) {
+        return cases.get(index);
+    }
+
+    public List<TestCase> asList() {
+        return cases;
+    }
 }
+
