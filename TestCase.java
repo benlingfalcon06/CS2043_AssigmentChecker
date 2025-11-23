80f7e0621919b610d3092c9ce6caf55624eeaa45
fmsl1380 <farbod.mosallaei@gmail.com>
2025-11-22 20:04:12 -0400

    Update TestCase.java

--- TestCase.java
+++ TestCase.java
@@ -1,1 +1,18 @@
+public class TestCase 
+{
+    private final String input;
+    private final String expectedOutput;
+
+    public TestCase(String input, String expectedOutput) 
+    {
+        this.input = input;
+        this.expectedOutput = expectedOutput;
+    }
+
+    public String getInput() { return input; }
+    public String getExpectedOutput() 
+    { 
+      return expectedOutput; 
+    }
+}
 
