552e25ba8bc87a841c9410aad02aa721b85eaed2
fmsl1380 <farbod.mosallaei@gmail.com>
2025-11-22 20:04:58 -0400

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,1 +1,28 @@
+import java.io.File;
+
+public class Program 
+{
+    private final File javaFile;
+
+    public Program(File javaFile) 
+  {
+        this.javaFile = javaFile;
+  }
+
+    public File getJavaFile() 
+    {
+        return javaFile;
+    }
+
+    public String getMainClassName() 
+    {
+        String name = javaFile.getName();
+        return name.substring(0, name.length() - 5); 
+    }
+
+    public String getClassPathDir() 
+    {
+        return javaFile.getParent();
+    }
+}
 
