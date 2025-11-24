e7397d92060d4acd24c21901e8f77551260fa7a9
fmsl1380 <farbod.mosallaei@gmail.com>
2025-11-23 23:34:09 -0400

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,28 +1,37 @@
 import java.io.File;
 
-public class Program 
-{
-    private final File javaFile;
+public class Program {
+    private final String name;   // program name = folder name
+    private final File javaFile; // the .java file containing main
 
-    public Program(File javaFile) 
-  {
+    public Program(String name, File javaFile) {
+        this.name = name;
         this.javaFile = javaFile;
-  }
+    }
 
-    public File getJavaFile() 
-    {
+    public String getName() {
+        return name;
+    }
+
+    public File getJavaFile() {
         return javaFile;
     }
 
-    public String getMainClassName() 
-    {
-        String name = javaFile.getName();
-        return name.substring(0, name.length() - 5); 
+    public String getMainClassName() {
+        // class name assumed to be file name without .java
+        String fileName = javaFile.getName();
+        if (fileName.endsWith(".java")) {
+            return fileName.substring(0, fileName.length() - 5);
+        }
+        return fileName;
     }
 
-    public String getClassPathDir() 
-    {
-        return javaFile.getParent();
+    public String getClassPathDir() {
+        return javaFile.getParentFile().getAbsolutePath();
+    }
+
+    @Override
+    public String toString() {
+        return "Program{" + name + ", file=" + javaFile.getAbsolutePath() + "}";
     }
 }
-
