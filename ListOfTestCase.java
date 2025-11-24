70945abd07138095fd7526f65d4170b460d7c254
fmsl1380 <farbod.mosallaei@gmail.com>
2025-11-23 23:33:26 -0400

    Update ListOfTestCase.java

--- ListOfTestCase.java
+++ ListOfTestCase.java
@@ -1,18 +1,35 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListOfTestSuites {
-    private final List<TestSuit> suites = new ArrayList<>();
+public class ListOfTestCase {
+    private final List<TestCase> cases;
 
-    public void add(TestSuit ts) 
-    {
-        suites.add(ts); 
+    public ListOfTestCase() {
+        this.cases = new ArrayList<>();
     }
-    public TestSuit get(int i) 
-    { 
-        return suites.get(i); 
+
+    public void add(TestCase t) {
+        cases.add(t);
     }
-    public int size() 
-    { return suites.size(); 
+
+    public int size() {
+        return cases.size();
+    }
+
+    public TestCase get(int index) {
+        return cases.get(index);
+    }
+
+    public List<TestCase> asList() {
+        return cases;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (TestCase tc : cases) {
+            sb.append(tc.toString()).append(System.lineSeparator());
+        }
+        return sb.toString();
     }
 }
