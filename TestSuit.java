b4b25b05c2d7dce2b9955fc7de56f5fbfc86e9d7
RetroR1xE <118540595+RetroR1xE@users.noreply.github.com>
2025-11-24 00:44:00 -0400

    TestSuite with functions from the requirements doc

--- TestSuit.java
+++ TestSuit.java
@@ -1,45 +1,33 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-/*********************************************************************************
- * This class defines test suites for grouping multiple test cases and addding them
- * 
- * author: Atharva Naik
- * Version: 1.0
- * insitally created:  Novemeber 18, 2025
- ******************************************************************************/
-
-public class TestSuit
+public class TestSuite
 {
     // Instance Variables
     private String name;
-    private ListOfTestCases testCases = new ListOfTestCases();
-    
-    // Constructor
+    private ListOfTestCase testCases = new ListOfTestCase();
+
+    // Req FR-1.2 – create a test suite (group of test cases)
     public TestSuite(String name) 
     {
         this.name = name;
     }
 
-    // Method to add test case to the test suite
+    // Req FR-1.1 – add a test case to a test set/suite
     public void addTestCase(TestCase tc) 
     {
         testCases.add(tc);
     }
 
+    // Req FR-1.1 – remove a test case from the suite
     public void removeTestCase(TestCase tc) 
     {
         testCases.remove(tc);
     }
 
-
-    // Getter for test cases
-    public ListOfTestCases getTestCases() 
+    // Req FR-7.3 – allow other components to list/search test cases
+    public ListOfTestCase getTestCases() 
     {
         return testCases;
     }
-
-
-
-
 }
