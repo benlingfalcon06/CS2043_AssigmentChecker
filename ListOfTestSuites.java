42f9c211c50fd4ad46a681d75cad2426ff97e5ac
fmsl1380 <farbod.mosallaei@gmail.com>
2025-11-23 23:33:54 -0400

    Update ListOfTestSuites.java

--- ListOfTestSuites.java
+++ ListOfTestSuites.java
@@ -1,33 +1,15 @@
+public class ListOfTestSuites {
+    private TestSuit singleSuite;
 
-/***************************************************
- * This class is the list of test suites
- * 
- * 
-  * author: Atharva Naik
- * Version: 1.0
- * insitally created:  Novemeber 18, 2025
- *******************************************************/
-
-public class ListOfTestSuites 
-{
-    private ArrayList<TestSuite> suites = new ArrayList<>();
-
-    public void add(TestSuite suite) 
-    {
-
-        suites.add(suite);
+    public void setSuite(TestSuit suite) {
+        this.singleSuite = suite;
     }
 
-    public TestSuite find(String name) 
-    {
-        for (TestSuite suite : suites) 
-        {
-            if (suite.getName().equals(name)) 
-            {
-                return suite;
-            }
-        }
-        
-        return "Not Found";
+    public TestSuit getSuite() {
+        return singleSuite;
+    }
+
+    public int size() {
+        return (singleSuite == null) ? 0 : 1;
     }
 }
